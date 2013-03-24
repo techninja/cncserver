bb1097b6b24aaf1d6d327a58598e9485a5a08a67
techninja <james@tn42.com>
2013-03-24 14:21:11 -0700

    Speed up the default WaterColorBot config a bit

--- cncserver.js
+++ cncserver.js
@@ -23,8 +23,8 @@ config = {
   name: 'WaterColorBot',
   maxArea: {width: 12420, height: 7350}, // Size in steps
   workArea: {top: 0, left: 2450}, // Size in steps
-  drawSpeed: 1000, // Drawing (brush down) speed in steps per second
-  moveSpeed: 1500, // Moving (brush up) speed in steps per second
+  drawSpeed: 2000, // Drawing (brush down) speed in steps per second
+  moveSpeed: 3000, // Moving (brush up) speed in steps per second
   servo: {
     min: 12900, // Brush Lift amount (lower number lifts higher)
     max: 18000  // Brush fall (servo arm stays clear)
