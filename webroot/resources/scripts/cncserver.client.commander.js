3580a8f257c3fe088a43947e466e78e19a0e15af
techninja <james@tn42.com>
2013-04-09 02:48:17 -0700

    Two new files: commander.js, & paths.js to handle command buffers and paths

---
+++ webroot/resources/scripts/cncserver.client.commander.js
@@ -0,0 +1,12 @@
+/**
+ * @file Holds all CNC Server command abstractions for API shortcuts. The API
+ * Makes the actual commands to the server, but this manages their execution and
+ * buffering to avoid collisions.
+ *
+ * Only applies to specific API functions that require waiting for the bot to
+ * finish, handles all API callbacks internally.
+ */
+
+cncserver.cmd = {
+
+};
---
+++ webroot/resources/scripts/cncserver.client.paths.js
@@ -0,0 +1,7 @@
+/**
+ * @file Holds all CNC Server path management and tracing functions
+ */
+
+cncserver.paths = {
+
+};
