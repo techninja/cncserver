4e31980353a5da8c012ebf40381ad510916adcca
techninja <james@tn42.com>
2013-04-09 16:52:42 -0700

    Add simple path outline overshoot for brush dynamics. Doesn't really deal with IN path overshoot, but it sure helps path gapping

--- webroot/resources/scripts/cncserver.client.paths.js
+++ webroot/resources/scripts/cncserver.client.paths.js
@@ -27,10 +27,16 @@ cncserver.paths = {
     cncserver.state.process.waiting = true;
 
     var i = 0;
+    var lastPoint = {};
+    var p = {};
+    var delta = {};
     function runNextPoint() {
       if (i <= $path.maxLength) {
         i+= cncserver.config.precision;
-        var p = $path.getPoint(i);
+
+        lastPoint = {x:p.x, y:p.y}; // Store the last run point
+        p = $path.getPoint(i); // Get a new point
+        delta = {x:lastPoint.x - p.x, y: lastPoint.y - p.y} // Store the difference
 
         // If the path is still visible here
         if (cncserver.paths.getPointPathCollide(p) == $path[0]){
@@ -44,12 +50,23 @@ cncserver.paths = {
           }
         } else { // Path is invisible, lift the brush if we're not already waiting
           if (!cncserver.state.process.waiting) {
+            // Figure out how much change since last point, move more before lifting
+            if (delta.x || delta.y) {
+              var o = {x: p.x - (delta.x * 5), y: p.y - (delta.y * 5)};
+              run('move', o); // Overshoot to make up for brush flexibility
+            }
+
             run('up');
             cncserver.state.process.waiting = true;
           }
         }
         setTimeout(runNextPoint, 1);
       } else { // Done
+        // Figure out how much change since last point, move more before lifting
+        if (delta.x || delta.y) {
+          var o = {x: p.x - (delta.x * 5), y: p.y - (delta.y * 5)};
+          run('move', o); // Overshoot to make up for brush flexibility
+        }
         run([
           'up',
           'logdone'
