abdf3f82e9a3d1de9d2af8418704e9f5bda65bd0
techninja <james@tn42.com>
2013-10-11 15:36:51 -0700

    Neuter wipe routines as they're incomplete, but leave in for now.

--- cncserver.js
+++ cncserver.js
@@ -527,6 +527,7 @@ function serialPortReadyCallback() {
   }
 
   // Tool change
+  exports.setTool = setTool;
   function setTool(toolName, callback) {
     var tool = botConf.get('tools:' + toolName);
 
@@ -536,8 +537,13 @@ function serialPortReadyCallback() {
     // TODO: fold this into bot specific tool change logic
     var downHeight = toolName.indexOf('water') != -1 ? 'wash' : 'paint';
 
+    // If the last tool was water, give it a wipe
+    // TODO: This is left annoying AT wipe height between tool changes, and
+    // should be heavily refactored before being used.
+    var moveHeight = pen.tool.indexOf('water') != -1 ? 'wipe' : 'up';
+
     // Pen Up
-    setHeight('up', function(){
+    setHeight(moveHeight, function(){
       // Move to the tool
       movePenAbs(tool, function(data){
         // Pen down
