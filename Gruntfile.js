0fd75b7aa2a6261cd7082daa6fb1ade08292c5d1
techninja <james@tn42.com>
2016-01-03 14:32:35 -0800

    Add forgotten gruntfile for running tests, thanks @jleverenz! CC #67

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+/*jslint node: true */
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: { reporter: 'spec' },
+        src: ['test/*.js']
+      }
+    },
+    jshint: {
+      all: ['*.js', 'test/*.js'],
+      options:{
+        jshintrc: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
+};
