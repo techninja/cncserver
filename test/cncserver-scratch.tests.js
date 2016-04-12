47cfc8e9aa90401131ac321032fe93052b00eea5
techninja <james@tn42.com>
2016-04-11 17:01:59 -0700

    Make sure scratch tests are clean and working

--- test/cncserver-scratch.tests.js
+++ test/cncserver-scratch.tests.js
@@ -20,10 +20,23 @@ describe('ScratchTests', function(){
 
   describe('Init start', function(){
     it('should be on by default', function(){
-      expect(cncserver.said('Scratch v2 Programming support ENABLED')).to.equal(true);
+      expect(
+        cncserver.said('Scratch v2 Programming support ENABLED')
+      ).to.equal(true);
     });
 
-    it('[coord] should move to the center from park (3833, 1800)', function(done){
+    it('[pen.up] put the pen up for the rest of the way', function(done){
+      req.get(path + 'pen.up').on('response', function(){
+        setTimeout(function(){
+          expect(cncserver.out[2]).to.contain('SC,5,19750'); // Set Position
+          expect(cncserver.out[1]).to.contain('SP,0'); // Go to position
+          expect(cncserver.out[0]).to.contain('SM,384,0,0'); // Block till done.
+          done();
+        }, 150); // Eat up the whole time taken for move
+      });
+    });
+
+    it('[coord] move to the center from park (3833, 1800)', function(done){
       req.get(path + 'coord/0/0').on('response', function(){
         isCentered = true;
         setTimeout(function(){
@@ -41,7 +54,7 @@ describe('ScratchTests', function(){
   });
 
   describe('Basic Moving (forward, right, left, toward, absturn)', function(){
-    it('[forward] should move 10 steps (100 steppers steps right)', function(done){
+    it('[forward] move 10 steps (100 steppers steps right)', function(done){
       req.get(path + 'move.forward./10').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[0]).to.contain('SM,67,100,0');
@@ -52,14 +65,14 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[right] should turn right 90deg to 90deg (no movement)', function(done){
+    it('[right] turn right 90deg to 90deg (no movement)', function(done){
       req.get(path + 'move.right./90').on('response', function(){
         expect(cncserver.out[0]).to.equal('Rotate right 90 deg. to 90 deg.');
         done();
       });
     });
 
-    it('[forward] should move 10 steps down (100 steppers steps)', function(done){
+    it('[forward] move 10 steps down (100 steppers steps)', function(done){
       req.get(path + 'move.forward./10').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[0]).to.contain('SM,67,0,100');
@@ -69,14 +82,14 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[right] should turn right 90deg to 180deg (no movement)', function(done){
+    it('[right] turn right 90deg to 180deg (no movement)', function(done){
       req.get(path + 'move.right./90').on('response', function(){
         expect(cncserver.out[0]).to.equal('Rotate right 90 deg. to 180 deg.');
         done();
       });
     });
 
-    it('[forward] should move 10 steps left (100 steppers steps)', function(done){
+    it('[forward] move 10 steps left (100 steppers steps)', function(done){
       req.get(path + 'move.forward./10').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[0]).to.contain('SM,67,-100,0');
@@ -86,7 +99,7 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[right] should turn right 90deg to 270deg (no movement)', function(done){
+    it('[right] turn right 90deg to 270deg (no movement)', function(done){
       req.get(path + 'move.right./90').on('response', function(){
         expect(cncserver.out[0]).to.equal('Rotate right 90 deg. to 270 deg.');
         done();
@@ -110,7 +123,7 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[forward] should move 10 steps left (100 steppers steps)', function(done){
+    it('[forward] move 10 steps left (100 steppers steps)', function(done){
       req.get(path + 'move.forward./10').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[0]).to.contain('SM,67,-100,0');
@@ -120,21 +133,25 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[toward] should turn towards (0,0) to 0deg (no movement)', function(done){
+    it('[toward] turn towards (0,0) to 0deg (no movement)', function(done){
       req.get(path + 'move.toward./0/0').on('response', function(){
-        expect(cncserver.out[0]).to.equal('Rotate relative towards 3832.5,1800 from 3733, 1800 to 0 deg');
+        expect(cncserver.out[0]).to.equal(
+          'Rotate relative towards 3832.5,1800 from 3733, 1800 to 0 deg'
+        );
         done();
       });
     });
 
-    it('[absturn] should turn to 270deg (no movement)', function(done){
+    it('[absturn] turn to 270deg (no movement)', function(done){
       req.get(path + 'move.absturn./270').on('response', function(){
-        expect(cncserver.out[0]).to.equal('Rotate to 270 scratch degrees (actual angle 180deg)');
+        expect(cncserver.out[0]).to.equal(
+          'Rotate to 270 scratch degrees (actual angle 180deg)'
+        );
         done();
       });
     });
 
-    it('[forward] should move -10 steps toward right (-100 steppers steps)', function(done){
+    it('[forward] move -10 steps right (-100 steppers steps)', function(done){
       req.get(path + 'move.forward./-10').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[0]).to.contain('SM,67,100,0');
@@ -145,7 +162,7 @@ describe('ScratchTests', function(){
     });
 
     it('[poll] be in a specific location/angle', function(done){
-      req(path + 'poll', function(err, response, body){
+      req(path + 'poll', function(err, response, body){ // jshint unused:false
         expect(body).to.contain('x 0.05'); // Rounding error? :P
         expect(body).to.contain('y 0');
         expect(body).to.contain('angle 270');
@@ -157,34 +174,38 @@ describe('ScratchTests', function(){
   //============================================================================
   //============================================================================
 
-  describe('Drawing outside boundary (forward, left, absturn, pen.down)', function(){
+  describe('Draw outside area (forward, left, absturn, pen.down)', function(){
     it('[absturn] should turn to 0deg (no movement)', function(done){
       req.get(path + 'move.absturn./0').on('response', function(){
-        expect(cncserver.out[0]).to.equal('Rotate to 0 scratch degrees (actual angle -90deg)');
+        expect(cncserver.out[0]).to.equal(
+          'Rotate to 0 scratch degrees (actual angle -90deg)'
+        );
         done();
       });
     });
 
-    it('[pen.down] should put the pen down for drawing', function(done){
+    it('[pen.down] put the pen down for drawing', function(done){
       req.get(path + 'pen.down').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[2]).to.contain('SC,5,12750'); // Set Position
           expect(cncserver.out[1]).to.contain('SP,0'); // Go to position
-          expect(cncserver.out[0]).to.contain('SM,67,0,0'); // Block until it's done moving
+          expect(cncserver.out[0]).to.contain('SM,136,0,0'); // Block till done.
           done();
         }, 15); // Eat up the whole time taken for move
       });
     });
 
-    it('[forward] should attempt to move 500 steps, lifting  ("5000" stepper steps, limited to 1800 steps)', function(done){
+    it('[forward] attempt to move 500 steps, limited by area', function(done){
       req.get(path + 'move.forward./500').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[5]).to.equal('Move pen by 500 steps');
-          expect(cncserver.out[4]).to.contain('SM,1200,0,-1800'); // Move position
-          expect(cncserver.out[3]).to.equal('Skipping buffer to set height: 19750'); // Buffer skipped lift insertion
+          expect(cncserver.out[4]).to.contain('SM,1200,0,-1800');
+          expect(cncserver.out[3]).to.equal(
+            'Skipping buffer to set height: 19750' // Buffer skipped lift
+          );
           expect(cncserver.out[2]).to.contain('SC,5,19750'); // Set Position
           expect(cncserver.out[1]).to.contain('SP,0'); // Go to position
-          expect(cncserver.out[0]).to.contain('SM,137,0,0'); // Block till complete
+          expect(cncserver.out[0]).to.contain('SM,136,0,0'); // Block till done.
 
           done();
         }, 1500); // Eat up the whole time taken to move there
@@ -192,7 +213,7 @@ describe('ScratchTests', function(){
     });
 
     it('[poll] should be at -500 y, pointing up (off canvas)', function(done){
-      req(path + 'poll', function(err, response, body){
+      req(path + 'poll', function(err, response, body){ // jshint unused:false
         expect(body).to.contain('y -500');
         expect(body).to.contain('angle 0');
         done();
@@ -200,14 +221,14 @@ describe('ScratchTests', function(){
     });
 
 
-    it('[left] should turn left 90deg to 180deg (left, no movement)', function(done){
+    it('[left] turn left 90deg to 180deg (left, no movement)', function(done){
       req.get(path + 'move.left./90').on('response', function(){
         expect(cncserver.out[0]).to.equal('Rotate left 90 deg. to 180 deg.');
         done();
       });
     });
 
-    it('[forward] should move 20 steps left (200 stepper steps)', function(done){
+    it('[forward] move 20 steps left (200 stepper steps)', function(done){
       req.get(path + 'move.forward./20').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[1]).to.equal('Move pen by 20 steps');
@@ -217,7 +238,7 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[left] should turn left 90deg to 90deg (down, no movement)', function(done){
+    it('[left] turn left 90deg to 90deg (down, no movement)', function(done){
       req.get(path + 'move.left./90').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[0]).to.equal('Rotate left 90 deg. to 90 deg.');
@@ -226,7 +247,7 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[forward] should move 100 steps down (0 actual stepper steps)', function(done){
+    it('[forward] move 100 steps down (0 actual stepper steps)', function(done){
       req.get(path + 'move.forward./100').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[1]).to.equal('Move pen by 100 steps');
@@ -236,15 +257,15 @@ describe('ScratchTests', function(){
       });
     });
 
-    it('[forward] should move 400 steps down (1800 actual stepper steps)', function(done){
+    it('[forward] move 400 steps down (1800 stepper steps)', function(done){
       req.get(path + 'move.forward./400').on('response', function(){
         setTimeout(function(){
           expect(cncserver.out[5]).to.equal('Move pen by 400 steps');
-          expect(cncserver.out[4]).to.equal('Go back to: draw'); // Move to "last" pen height
+          expect(cncserver.out[4]).to.equal('Go back to: draw'); // last height
           expect(cncserver.out[3]).to.contain('SC,5,12750'); // Set Position
           expect(cncserver.out[2]).to.contain('SP,0'); // Go to position
-          expect(cncserver.out[1]).to.contain('SM,137,0,0'); // Block till complete
-          expect(cncserver.out[0]).to.contain('SM,1200,0,1800'); // Move position
+          expect(cncserver.out[1]).to.contain('SM,136,0,0'); // Block till done
+          expect(cncserver.out[0]).to.contain('SM,1200,0,1800'); // Move
           done();
         }, 1800); // Eat up the whole time taken to move there
       });
