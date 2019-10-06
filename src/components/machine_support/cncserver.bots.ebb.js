d48637f651c5c1f6705b58c6b03480025b33d3db
techninja <james@tn42.com>
2019-10-06 12:44:05 -0700

    Add support for EBB firmware version checking and WIP emergency stop code (not working)

--- src/components/machine_support/cncserver.bots.ebb.js
+++ src/components/machine_support/cncserver.bots.ebb.js
@@ -2,29 +2,106 @@
  * @file Abstraction module for EiBotBoard specific support.
  * @see http://evil-mad.github.io/EggBot/ebb.html
  */
-const ebb = { id: 'ebb' }; // Exposed export.
+const semver = require('semver');
+
+const ebb = { id: 'ebb', version: {} }; // Exposed export.
+const minVersion = '>=2.2.7';
+let controller = {}; // Placeholder for machine config export.
 
 module.exports = (cncserver) => {
-
-
-  // Bind EBB support on controller setup before serial connection.
+  // Bind EBB support on controller setup -before- serial connection.
   cncserver.binder.bindTo('controller.setup', ebb.id, (controller) => {
     if (controller.name === 'EiBotBoard') {
-      const rate = cncserver.settings.botConf.get('servo:rate');
       cncserver.serial.setSetupCommands([
         // Set motor precision.
         cncserver.buffer.cmdstr(
           'enablemotors',
           { p: cncserver.settings.botConf.get('speed:precision') }
         ),
-
-        // TODO: This probably shouldn't be set twice.
-        cncserver.buffer.cmdstr('configureservo', { r: rate }),
-        cncserver.buffer.cmdstr('configureservo', { r: rate }),
       ]);
     }
   });
 
+  // Bind to serial connection.
+  cncserver.binder.bindTo('serial.connected', ebb.id, () => {
+    // Exit early if we've already done this (happens on reconnects).
+    if (ebb.version.value) {
+      return;
+    }
+
+    controller = cncserver.settings.botConf.get('controller');
+
+    // Get EBB version.
+    cncserver.ipc.getSerialValue('version').then((message) => {
+      if (message.includes(controller.error)) {
+        console.error('='.repeat(76));
+        console.error('Failed to load version information! Message given:');
+        console.error(`"${message}"`);
+        console.error('Please restart CNC Server to ensure correct operation.');
+        console.error('='.repeat(76));
+        return;
+      }
+
+      const version = message.split(' ').pop();
+      ebb.version = {
+        value: version,
+        string: message,
+      };
+      console.log(`Connected to ${controller.manufacturer} ${controller.name}, firmware v${version}`);
+      if (!semver.satisfies(version, minVersion)) {
+        console.error('='.repeat(76));
+        console.error(`ERROR: Firmware version does not meet minimum requirements (${minVersion})`);
+        console.error('To upgrade, see: https://wiki.evilmadscientist.com/Updating_EBB_firmware');
+        console.error('='.repeat(76));
+      }
+    });
+  });
+
+
+  // Bind to general controller message returns.
+  cncserver.binder.bindTo('serial.message', ebb.id, (message) => {
+    // If this isn't an acknowledgement of a working command...
+    if (message !== controller.ack) {
+      if (message.includes(controller.error)) {
+        console.error('Error from EBB:', message);
+      }
+    }
+  });
+
+  /* // Bind to buffer clear to emergency stop.
+  // TODO: This does not work, but it SHOULD. During moves, we need to find out
+  // how far we failed to go before it stopped, which IS reported, but I suspect
+  // these are RAW motor steps before X/Y (XM) moves, so this doesn't work for
+  // AxiDraw or anything else that doesn't use standard direct X/Y moves.
+  // Likely what needs to happen here is we do the conversion ourselves.
+  cncserver.binder.bindTo('buffer.clear', ebb.id, () => {
+    cncserver.ipc.getSerialValue('estop').then((message) => {
+      // @see: http://evil-mad.github.io/EggBot/ebb.html#ES
+      const data = message.split(',');
+
+      // Only if the motors were moving should we fix the position.
+      if (data[0] === '1') {
+        const offset = { x: parseInt(data[3], 10), y: parseInt(data[4], 10) };
+        console.log(data);
+        console.log(offset);
+        const ap = cncserver.actualPen;
+        console.log(ap.state);
+        const force = {
+          x: ap.state.x + (offset.x + offset.y),
+          y: ap.state.y + (offset.y - offset.x),
+          lastDuration: 0,
+        };
+
+        console.log(force);
+
+        cncserver.pen.forceState(force);
+        ap.forceState(force);
+
+        // console.dir(ap.state);
+      }
+    });
+  });
+  //* /
 
   /**
    * Run to the buffer direct low level setup commands (for EiBotBoard only).
