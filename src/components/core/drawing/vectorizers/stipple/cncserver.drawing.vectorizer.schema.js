c5063ac43162c2149038b5f1b4d0fee0169a4844
techninja <james@tn42.com>
2020-04-16 14:12:42 -0700

    Move stipple vectorizer over to new home (linux only still)

---
+++ src/components/core/drawing/vectorizers/stipple/cncserver.drawing.vectorizer.schema.js
@@ -0,0 +1,9 @@
+/**
+ * @file Settings schema for vectorizer method application.
+ *
+ * This schema defines the vectorizer method specific settings schema for the
+ * application to import and use for settings validation and documentation.
+ */
+module.exports = {
+
+};
