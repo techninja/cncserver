3382224a3d4a2ab1ce1b683cef294d7f2af42cdc
techninja <james@tn42.com>
2020-04-16 14:53:00 -0700

    Complete working stipple vectorizer with schema

--- src/components/core/drawing/vectorizers/stipple/cncserver.drawing.vectorizer.schema.js
+++ src/components/core/drawing/vectorizers/stipple/cncserver.drawing.vectorizer.schema.js
@@ -4,6 +4,48 @@
  * This schema defines the vectorizer method specific settings schema for the
  * application to import and use for settings validation and documentation.
  */
+/* eslint-disable max-len */
 module.exports = {
-
+  useColor: {
+    type: 'boolean',
+    title: 'Use Color',
+    description: 'Parse color from the raster into the stipple points, otherwise just greyscale.',
+    default: false,
+  },
+  noOverlap: {
+    type: 'boolean',
+    title: 'No Overlap',
+    description: 'Prevent stipple points from overlapping each other.',
+    default: false,
+  },
+  fixedRadius: {
+    type: 'boolean',
+    title: 'Fixed radius',
+    description: 'Lock the radius of the stipple point circles to a single size.',
+    default: false,
+  },
+  points: {
+    type: 'integer',
+    title: 'Stipple points',
+    description: 'Number of points to fit against the raster.',
+    default: 500,
+    minimum: 20,
+    maximum: 80000,
+  },
+  sizingFactor: {
+    type: 'number',
+    title: 'Sizing Factor',
+    description: 'Final stipple point circle radius multiplier.',
+    default: 1,
+    minimum: 0.1,
+    maximum: 10,
+  },
+  subpixels: {
+    type: 'integer',
+    title: 'Subpixels',
+    description: 'Tile size of centroid computations.',
+    default: 5,
+    minimum: 1,
+    maximum: 20,
+  },
 };
--- src/components/core/drawing/vectorizers/stipple/cncserver.drawing.vectorizers.stipple.js
+++ src/components/core/drawing/vectorizers/stipple/cncserver.drawing.vectorizers.stipple.js
@@ -7,25 +7,12 @@ const { exec } = require('child_process');
 const { tmpdir } = require('os');
 const fs = require('fs');
 const path = require('path');
-const dataUriToBuffer = require('data-uri-to-buffer');
-const util = require('./cncserver.drawing.vectorizers.util');
+const util = require('../cncserver.drawing.vectorizers.util');
 
 const bin = path.resolve(__dirname, 'bin', process.platform, 'voronoi_stippler');
-const tmp = path.resolve(tmpdir());
+const tmp = tmpdir();
 
-let settings = {
-  useColor: false,
-  noOverlap: false,
-  fixedRadius: false,
-  points: 500,
-  maxDensity: 4,
-  sizingFactor: 1,
-  brightness: 0.05,
-  rasterDPI: 300,
-  subPixels: 5,
-  input: path.resolve(tmp, 'stipple_input.png'),
-  output: path.resolve(tmp, 'stipple_output.svg'),
-};
+let settings = { }; // Globalize settings.vectorize >
 
 // Map settings values to verbose command line options.
 const optionMap = {
@@ -36,20 +23,8 @@ const optionMap = {
   noOverlap: 'no-overlap',
   fixedRadius: 'fixed-radius',
   sizingFactor: 'sizing-factor',
-  subPixels: 'subpixels',
+  subpixels: 'subpixels',
 };
-
-const writeImage = (source, dest) => new Promise((success, error) => {
-  const exportRaster = source.rasterize(settings.rasterDPI);
-  const b = dataUriToBuffer(exportRaster.toDataURL());
-  fs.writeFile(dest, b, (err) => {
-    if (err) {
-      error(err);
-    } else {
-      success();
-    }
-  });
-});
 
 const buildOptions = () => {
   const opts = [];
@@ -66,57 +41,55 @@ const buildOptions = () => {
 };
 
 // Connect to the main process, start the vectorization operation.
-util.connect((image, settingsOverride) => {
-  settings = { ...settings, ...settingsOverride };
-  const { input, output } = settings;
-
-  // How does this work?
-  // - Take in the image, size/brighten appropriately.
-  // - Rasterize image and save PNG
-  // - Run the command
-  // - When it finishes, import the SVG
+util.connect('png', (input, rawSettings) => {
+  const output = path.resolve(tmp, `stipple_output_${util.info.hash}.svg`);
+  settings = {
+    ...rawSettings.stipple,
+    input,
+    output,
+  };
 
   // Remove any previous run output file.
-  if (fs.existsSync(output)) {
-    fs.unlinkSync(output);
-  }
+  if (fs.existsSync(output)) fs.unlinkSync(output);
 
-  // Write the PNG for input.
-  writeImage(image, input).then(() => {
-    const opts = buildOptions();
-    const fullExec = `${bin} ${opts.join(' ')}`;
-    console.log('Executing:', fullExec);
-    const child = exec(fullExec);
+  // Push the input image and spawn the child process.
+  const opts = buildOptions();
+  const fullExec = `${bin} ${opts.join(' ')}`;
+  console.log('Executing:', fullExec);
+  const child = exec(fullExec);
 
-    child.stdout.on('data', (data) => {
-      if (data.includes('% Complete')) {
-        const progress = Math.min(100, parseInt(data.split('%')[0], 10));
-        console.log('Progres:', progress);
-      }
-    });
+  child.stderr.on('data', (data) => {
+    console.error('ERROR:', data);
+  });
 
-    child.on('close', () => {
-      if (fs.existsSync(output)) {
-        console.log('Importing file', output);
+  child.stdout.on('data', (data) => {
+    if (data.includes('% Complete')) {
+      const progress = Math.min(100, parseInt(data.split('%')[0], 10));
+      console.log('Progress:', progress);
+    }
+  });
 
-        util.project.importSVG(output, {
-          expandShapes: true,
-          onLoad: (group) => {
-            group.fitBounds(image.bounds);
+  child.on('close', () => {
+    if (fs.existsSync(output)) {
+      console.log('Importing file', output);
 
-            // Should be a flat list of circles converted to 4 segment paths.
-            group.children.forEach((item) => {
-              item.strokeColor = item.fillColor;
-              item.strokeWidth = 0.5;
-              item.fillColor = null;
-            });
-            util.finish(group);
-          },
-        });
-      } else {
-        console.log('No output file 😢', output);
-        util.finish(new Group());
-      }
-    });
+      util.project.importSVG(output, {
+        expandShapes: true,
+        onLoad: (group) => {
+          group.fitBounds(rawSettings.bounds);
+
+          // Should be a flat list of circles converted to 4 segment paths.
+          group.children.forEach((item) => {
+            item.strokeColor = item.fillColor;
+            item.strokeWidth = 0.5;
+            item.fillColor = null;
+          });
+          util.finish(group);
+        },
+      });
+    } else {
+      console.log('No output file 😢', output);
+      util.finish(new Group());
+    }
   });
 });
